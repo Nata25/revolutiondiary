b0e9eaa182bee2f941ca16914270ee449ab50e48
Nata25 <natalyas.ivanova@gmail.com>
2015-12-13 00:40:10 +0200

    Test if values are stored in sessionStorage. Reveal slope problems.

--- js/datepicker-main.js
+++ js/datepicker-main.js
@@ -1,10 +1,26 @@
-// set session storage
+// Document.ready()
 
-sessionStorage.setItem('name', 'Name');
-
-// initialize datepicker
 $(function() {
-  $("#datepicker").datepicker(
+
+  // set defaultDate in the session storage
+
+  sessionStorage.setItem('current-day',   $('#date').html() );
+  sessionStorage.setItem('current-month', $('#month').html() );
+
+  console.log(sessionStorage.getItem('current-day'));
+  console.log(sessionStorage.getItem('current-month'));
+
+// TESTING!!!
+
+  sessionStorage.setItem('greeting', 'hello!');
+  var test = sessionStorage.getItem('greeting');
+  $('#date').html(test);
+
+// END OF TESTING!!!
+
+  // initialize datepicker
+
+$("#datepicker").datepicker(
     {
       dateFormat: "d m",
       defaultDate: new Date(2013, 10, 21),
@@ -25,22 +41,28 @@ $(function() {
     // format date to paste into date-block
 
         if (month == 11) {
-          month = "ЛИСТОПАДА";
+          month = "листопада";
         }
         else if (month == 12) {
-          month = "ГРУДНЯ"
+          month = "грудня";
+        }
+        else if (month == 1){
+          month = "січня";
         }
         else {
-          month = "СІЧНЯ"
+          month = "лютого";
         }
 
-        $("#date").html(day);
-        $("#month").html(month);
+        console.log(sessionStorage.getItem('current-day'));
+        console.log(sessionStorage.getItem('current-month'));
 
-        var check = sessionStorage.getItem('name');
-        $('#yellow').html(check);
+        sessionStorage.setItem('current-day', day);
+        sessionStorage.setItem('current-month', month);
+        $("#date").html(sessionStorage.getItem('current-day'));
+        $("#month").html(sessionStorage.getItem('current-month'));
 
       }) // end of onSelect
+
     }); // end of .datekpicker()
 
 }); // end of ready
